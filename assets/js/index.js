51ad1ed238fa6b1ebb94d987472f7ab3c5ce7058
xSarscov <vargasernest7@gmail.com>
2023-12-24 01:33:23 -0600

    done :bat:

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const searchForm = document.querySelector('#search-form');
+    
+    searchForm.addEventListener('submit', (event) => {
+
+        const searchField = document.querySelector('#search-field').value;    
+        
+        if (event.submitter.name === 'btnK' && searchField.trim() === ''){
+            event.preventDefault(); 
+        }
+
+    });
+    
+});
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
