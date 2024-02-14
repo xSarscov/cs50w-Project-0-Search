76b5c25992e6d227b1fd0172358517abc4e76d07
xSarscov <vargasernest7@gmail.com>
2024-02-14 01:37:33 -0600

    :bat:

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const searchForm = document.querySelector('#search-form');
-    
+
     searchForm.addEventListener('submit', (event) => {
 
-        const searchField = document.querySelector('#search-field').value;    
-        
-        if (event.submitter.name === 'btnK' && searchField.trim() === ''){
-            event.preventDefault(); 
+        const searchField = document.querySelector('#search-field').value;
+
+        if (event.submitter.name === 'btnK' && searchField.trim() === '') {
+            event.preventDefault();
         }
 
     });
-    
+
 });
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
