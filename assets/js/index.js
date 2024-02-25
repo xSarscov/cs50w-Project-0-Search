b41b592a169f22dbde7ace9cf652d0592245a711
xSarscov <vargasernest7@gmail.com>
2024-02-25 04:06:04 -0600

    Add dark/light mode and responsive images :skull:

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if user typed anything in the search input, if not stop the form submit
     const searchForm = document.querySelector('#search-form');
 
     searchForm.addEventListener('submit', (event) => {
@@ -12,8 +13,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 
+    // Change the color mode of the Google logo based on the theme of the page
+    const googleImage = document.querySelector('#google-img');
+
+    const ChangeLogoColor = () => {
+        const isLightTheme = document.documentElement.getAttribute('data-bs-theme') === 'light';
+
+        if (isLightTheme) {
+            googleImage.src = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+            googleImage.srcset = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png 1.5x';
+        } else {
+            googleImage.src = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png';
+            googleImage.srcset = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_160x56dp.png 1.5x';
+        }
+    }
+
+    const toggleColorModeBtn = document.querySelector('#theme-toggle');
+    const getPageTheme = () => document.querySelector('html').getAttribute('data-bs-theme');
+    const getStoredTheme = localStorage.getItem('theme') || 'dark';
+    const setStoredTheme = theme => localStorage.setItem('theme', theme);
+    
+    const setTheme = theme => document.documentElement.setAttribute('data-bs-theme', theme);
+    
+    toggleColorModeBtn.addEventListener('click', () => {
+        const newTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+        setStoredTheme(newTheme);
+        toggleColorModeBtn.innerText = `Dark mode: ${newTheme === 'dark' ? 'on' : 'off'}`;
+        setTheme(newTheme);
+        ChangeLogoColor();
+    });
+    
+    setTheme(getStoredTheme);
+    ChangeLogoColor();
+
 });
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// Initialize bootstrap tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+    tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+
+
+
+
+
+
+
 
